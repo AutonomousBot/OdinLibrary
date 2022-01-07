6976a5db252a0e07ccf1ed19aa4bb30ce4cada65
fred <fredericnguyen777@gmail.com>
2022-01-06 19:53:29 -0500

    added button to add books and added click event for it to add a book to myLibrary.

--- script.js
+++ script.js
@@ -7,16 +7,23 @@ function Book() {
 
 function addBookToLibrary() {
   let bookInput = capitalize(prompt("Which book would you like to add?"))
+  if (!bookInput == "") {
+    myLibrary.push(bookInput)
+  }
 }
 
 // Capitalizes title.
 function capitalize(string) {
-  const title = string.split('')
+  let newTitle = [];
+  const title = string.split(' ')
   for (i = 0; i< title.length; i++) {
-    title[i].charAt(0).toUpperCase() + title[i].slice(1)
+  newTitle[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1)
   }
-  return title.join(" ")
+  return newTitle.join(" ")
 }
+
+const button = document.getElementById("addBook")
+button.onclick = addBookToLibrary;
 
 // Side stuff
 // Add conditionals to skip conjunctions/determiner. (make function with array)
